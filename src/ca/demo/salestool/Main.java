af73fe9a8da96e9d00006987d32971ad6dd131b5
Administrator <Administrator@10.0.0.196>
2017-07-18 09:45:15 +0000

    Initial commit of sample github project

---
+++ src/ca/demo/salestool/Main.java
@@ -0,0 +1,14 @@
+package ca.demo.salestool;
+public class Main {
+	public static void main(String[] args){
+		Test data = new Test();
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting(){
+	System.out.println("Hello");
+	System.out.println("Test");
+	System.out.println("Test1");
+	}
+}
