0516da6937b76247c801ca932ad72e6844ceae2f
iDashboarHub <Aghalya.Baskaran@cognizant.com>
2017-07-20 04:35:36 +0000

    Old user commit

--- src/ca/demo/iDashboardHub/Test.java
+++ src/ca/demo/iDashboardHub/Test.java
@@ -4,9 +4,8 @@ public class Test{
 
 	int data[]={0,4,42};
 	public void display(){
-		System.out.println("Sub Test");
-		System.out.println("Sub Test 123");
-		System.out.println("Merge Test");
-		System.out.println("First change");
+		for(int i :data){
+			System.out.println(data[i]);
+		}
 }
 }
